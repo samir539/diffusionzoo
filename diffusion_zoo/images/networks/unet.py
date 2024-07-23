c9fcb2b1b2d722db0cedcb8da6c4a032d27bbfd1
samir <sc9018@ic.ac.uk>
2024-07-23 18:34:43 +0100

    added sinusoidal position embeddings

--- diffusion_zoo/images/networks/unet.py
+++ diffusion_zoo/images/networks/unet.py
@@ -1,5 +1,26 @@
-#implementation of a simple unet with residual connections
+#implementation of a simple unet
+import equinox as eqx
+import jax
+import jax.numpy as jnp
+import jax.random as jr
+from einops import rearrange
 
+from collections.abc import Callable
+from typing import Optional, Union
+import math
 
+#position embedding
+class sinusoidalPositionEmbeddngs(eqx.Module):
+    embedding: jax.Array
+    
+    def __init__(self,dim):
+        half_dim = dim//2
+        embedding = math.log(10000)/(half_dim -1)
+        self._embedding = jnp.exp(jnp.arange(half_dim)*-embedding)
+        
+    def __call__(self,x):
+        embedding = x*self._embedding
+        embedding = jnp.concatenate((jnp.sin(embedding),jnp.cos(embedding)),axis=-1)
+        return embedding
 
 
